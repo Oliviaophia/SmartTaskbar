b69348611ca175224ae37113bfda16f8cd98b04a
Chanple <chanple@outlook.com>
2018-01-24 10:58:36 +0800

    Meaningless modification

---
+++ SmartTaskbar/SmartTaskbar.h
@@ -0,0 +1,25 @@
+#pragma once
+
+
+APPBARDATA msgData;
+
+WINDOWPLACEMENT placement;
+
+HWND maxWindow;
+
+POINT cursor;
+
+bool tryShowBar;
+
+bool isWin10;
+
+
+BOOL EnumWindowsProc(HWND hwnd, LPARAM lParam);
+
+bool IsCursorOverTaskbar();
+
+DWORD uwpPID;
+
+DWORD windowPID;
+
+bool SetuwpPID();
