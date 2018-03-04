b72ce780d50b06c73e0ded0acebfbac0a596219e
Chanple <chanple@outlook.com>
2018-03-04 20:27:26 +0800

    ~

--- SmartTaskbar/TaskbarSwitcher.cs
+++ SmartTaskbar/TaskbarSwitcher.cs
@@ -1,11 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
-using System.Linq;
 using System.Runtime.InteropServices;
-using System.Text;
-using System.Threading.Tasks;
 using static SmartTaskbar.SafeNativeMethods;
 namespace SmartTaskbar
 {
