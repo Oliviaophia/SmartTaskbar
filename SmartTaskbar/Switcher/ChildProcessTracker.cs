684b41a93a23882b75599c5ffc33406f43236362
Chanple <chanple@outlook.com>
2018-03-11 23:10:13 +0800

    整理

--- SmartTaskbar/Switcher/ChildProcessTracker.cs
+++ SmartTaskbar/Switcher/ChildProcessTracker.cs
@@ -8,15 +8,7 @@ namespace SmartTaskbar
     //https://stackoverflow.com/questions/3342941/kill-child-process-when-parent-process-is-killed
     class ChildProcessTracker
     {
-        public void AddProcess(Process process)
-        {
-            if (s_jobHandle != IntPtr.Zero)
-            {
-                bool success = AssignProcessToJobObject(s_jobHandle, process.Handle);
-                if (!success)
-                    return;
-            }
-        }
+        private readonly IntPtr s_jobHandle;
 
         public ChildProcessTracker()
         {
@@ -41,6 +33,14 @@ namespace SmartTaskbar
             }
         }
 
-        private readonly IntPtr s_jobHandle;
+        public void AddProcess(Process process)
+        {
+            if (s_jobHandle != IntPtr.Zero)
+            {
+                bool success = AssignProcessToJobObject(s_jobHandle, process.Handle);
+                if (!success)
+                    return;
+            }
+        }
     }
 }
