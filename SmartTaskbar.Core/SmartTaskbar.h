f5dbaa49206bdb0f22b4d533669d58e2be92470b
Chanple <chanple@outlook.com>
2018-05-13 22:45:18 +0800

    ~

---
+++ SmartTaskbar.Core/SmartTaskbar.h
@@ -0,0 +1,25 @@
+#pragma once
+namespace SmartTaskbar
+{
+    namespace Core 
+    {
+        public ref class DefaultAutoMode
+        {
+        public:
+            DefaultAutoMode();
+            ~DefaultAutoMode();
+
+        private:
+
+        };
+
+        DefaultAutoMode::DefaultAutoMode()
+        {
+        }
+
+        DefaultAutoMode::~DefaultAutoMode()
+        {
+        }
+    }
+}
+
