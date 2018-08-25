811eec2d4074f470c96bda5a9397882fe2cd2685
Chanple <chanple@outlook.com>
2018-08-25 22:13:21 +0800

    ~

--- SmartTaskbar/SystemTray.cs
+++ SmartTaskbar/SystemTray.cs
@@ -1,4 +1,8 @@
-﻿using System.Windows.Forms;
+﻿using System;
+using System.Diagnostics;
+using System.Drawing;
+using System.Windows.Forms;
+using SmartTaskbar.Properties;
 using static SmartTaskbar.SafeNativeMethods;
 
 namespace SmartTaskbar
@@ -19,8 +23,9 @@ namespace SmartTaskbar
         public SystemTray()
         {
             #region Initialization
-            ResourceCulture resource = new ResourceCulture();
-            System.Drawing.Font font = new System.Drawing.Font("Segoe UI", 9F);
+
+            var resource = new ResourceCulture();
+            var font = new Font("Segoe UI", 9F);
             about = new ToolStripMenuItem
             {
                 Text = resource.GetString(nameof(about)),
@@ -72,25 +77,26 @@ namespace SmartTaskbar
             {
                 ContextMenuStrip = contextMenuStrip,
                 Text = @"SmartTaskbar v1.1.8",
-                Icon = System.Environment.OSVersion.Version.Major.ToString() == "10" ? Properties.Resources.logo_32 : Properties.Resources.logo_blue_32,
+                Icon = Environment.OSVersion.Version.Major.ToString() == "10" ? Resources.logo_32 : Resources.logo_blue_32,
                 Visible = true
             };
+
             #endregion
 
             #region Load Event
 
-            about.Click += (s, e) => System.Diagnostics.Process.Start(@"https://github.com/ChanpleCai/SmartTaskbar/releases");
+            about.Click += (s, e) => Process.Start(@"https://github.com/ChanpleCai/SmartTaskbar/releases");
 
-            Properties.Settings.Default.PropertyChanged += (s, e) =>
+            Settings.Default.PropertyChanged += (s, e) =>
             {
-                Properties.Settings.Default.Save();
-                switch ((AutoModeType)Properties.Settings.Default.TaskbarState)
+                Settings.Default.Save();
+                switch ((AutoModeType) Settings.Default.TaskbarState)
                 {
                     case AutoModeType.Display:
                         smallIcon.Enabled = auto_display.Checked = true;
                         auto_size.Checked = false;
                         ChangeDisplayState();
-                        SetIconSize(Properties.Settings.Default.IconSize);
+                        SetIconSize(Settings.Default.IconSize);
                         break;
                     case AutoModeType.Size:
                         smallIcon.Enabled = auto_display.Checked = false;
@@ -107,15 +113,15 @@ namespace SmartTaskbar
 
             smallIcon.Click += (s, e) =>
             {
-                Properties.Settings.Default.IconSize = smallIcon.Checked ? 0 : 1;
-                SetIconSize(Properties.Settings.Default.IconSize);
+                Settings.Default.IconSize = smallIcon.Checked ? 0 : 1;
+                SetIconSize(Settings.Default.IconSize);
             };
 
             animation.Click += (s, e) => animation.Checked = ChangeTaskbarAnimation();
 
-            auto_size.Click += (s, e) => Properties.Settings.Default.TaskbarState = auto_size.Checked ? (int)AutoModeType.None : (int)AutoModeType.Size;
+            auto_size.Click += (s, e) => Settings.Default.TaskbarState = auto_size.Checked ? (int) AutoModeType.None : (int) AutoModeType.Size;
 
-            auto_display.Click += (s, e) => Properties.Settings.Default.TaskbarState = auto_display.Checked ? (int)AutoModeType.None : (int)AutoModeType.Display;
+            auto_display.Click += (s, e) => Settings.Default.TaskbarState = auto_display.Checked ? (int) AutoModeType.None : (int) AutoModeType.Display;
 
             exit.Click += (s, e) =>
             {
@@ -128,9 +134,7 @@ namespace SmartTaskbar
             notifyIcon.MouseClick += (s, e) =>
             {
                 if (e.Button != MouseButtons.Right)
-                {
                     return;
-                }
 
                 notifierLauncher.Resume();
 
@@ -139,38 +143,32 @@ namespace SmartTaskbar
                 smallIcon.Checked = GetIconSize() == SmallIcon;
 
                 if (smallIcon.Enabled)
-                {
-                    SetIconSize(Properties.Settings.Default.IconSize);
-                }
+                    SetIconSize(Settings.Default.IconSize);
             };
 
             notifyIcon.MouseDoubleClick += (s, e) =>
             {
-                Properties.Settings.Default.TaskbarState = (int)AutoModeType.None;
-                SetIconSize(Properties.Settings.Default.IconSize);
+                Settings.Default.TaskbarState = (int) AutoModeType.None;
+                SetIconSize(Settings.Default.IconSize);
                 if (IsHide())
-                {
                     Show();
-                }
                 else
-                {
                     Hide();
-                }
             };
 
             #endregion
 
             #region Load Settings
 
-            if (Properties.Settings.Default.TaskbarState == -1)
+            if (Settings.Default.TaskbarState == -1)
             {
                 //Run the software for the first time
-                Properties.Settings.Default.TaskbarState = (int)AutoModeType.Size;
-                Properties.Settings.Default.IconSize = GetIconSize();
+                Settings.Default.TaskbarState = (int) AutoModeType.Size;
+                Settings.Default.IconSize = GetIconSize();
             }
             else
             {
-                switch ((AutoModeType)Properties.Settings.Default.TaskbarState)
+                switch ((AutoModeType) Settings.Default.TaskbarState)
                 {
                     case AutoModeType.Display:
                         smallIcon.Enabled = auto_display.Checked = true;
@@ -185,6 +183,7 @@ namespace SmartTaskbar
                         auto_display.Checked = auto_size.Checked = false;
                         break;
                 }
+
                 Reset();
             }
 
