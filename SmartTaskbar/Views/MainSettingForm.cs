bf5f38476317ecef9edc229da4284f74704ede7c
Chanple <chanple@outlook.com>
2020-03-28 12:49:30 +0800

    ~

--- SmartTaskbar/Views/MainSettingForm.cs
+++ SmartTaskbar/Views/MainSettingForm.cs
@@ -1,7 +1,9 @@
-﻿using System.ComponentModel;
+﻿using System;
+using System.ComponentModel;
 using System.Drawing;
 using System.Windows.Forms;
 using SmartTaskbar.Core;
+using SmartTaskbar.Core.Settings;
 using SmartTaskbar.Model;
 
 namespace SmartTaskbar.Views
@@ -26,6 +28,8 @@ namespace SmartTaskbar.Views
 
             #region Initialization
 
+            #region Text
+
             groupBoxState0.Text = coreInvoker.GetText("SettTaskbarState0");
             groupBoxState1.Text = coreInvoker.GetText("SettTaskbarState1");
             groupBoxState2.Text = coreInvoker.GetText("SettTaskbarState2");
@@ -42,7 +46,7 @@ namespace SmartTaskbar.Views
 
             checkBoxHideTaskbar0.Text =
                 checkBoxHideTaskbar1.Text =
-                    checkBoxHideTaskbar2.Text = 
+                    checkBoxHideTaskbar2.Text =
                         coreInvoker.GetText("SettHideTaskbar");
 
             checkBoxIconSize0.Text =
@@ -66,6 +70,201 @@ namespace SmartTaskbar.Views
                         coreInvoker.GetText("SettBlur");
 
             #endregion
+
+            #region Event
+
+            checkBoxIsAutoHide0.CheckedChanged += (s, e) =>
+            {
+                _coreInvoker.UserSettings.ResetState.IsAutoHide = checkBoxIsAutoHide0.Checked;
+                _coreInvoker.SaveUserSettings();
+            };
+
+            checkBoxIsAutoHide1.CheckedChanged += (s, e) =>
+            {
+                _coreInvoker.UserSettings.ReadyState.IsAutoHide = checkBoxIsAutoHide1.Checked;
+                _coreInvoker.SaveUserSettings();
+            };
+
+            checkBoxIsAutoHide2.CheckedChanged += (s, e) =>
+            {
+                _coreInvoker.UserSettings.TargetState.IsAutoHide = checkBoxIsAutoHide2.Checked;
+                _coreInvoker.SaveUserSettings();
+            };
+
+            checkBoxHideTaskbar0.CheckedChanged += (s, e) =>
+            {
+                _coreInvoker.UserSettings.ResetState.HideTaskbarCompletely = checkBoxHideTaskbar0.Checked;
+                _coreInvoker.SaveUserSettings();
+            };
+
+            checkBoxHideTaskbar1.CheckedChanged += (s, e) =>
+            {
+                _coreInvoker.UserSettings.ReadyState.HideTaskbarCompletely = checkBoxHideTaskbar1.Checked;
+                _coreInvoker.SaveUserSettings();
+            };
+
+            checkBoxHideTaskbar2.CheckedChanged += (s, e) =>
+            {
+                _coreInvoker.UserSettings.TargetState.HideTaskbarCompletely = checkBoxHideTaskbar2.Checked;
+                _coreInvoker.SaveUserSettings();
+            };
+
+            checkBoxIconSize0.CheckedChanged += (s, e) =>
+            {
+                _coreInvoker.UserSettings.ResetState.IconSize = checkBoxIconSize0.Checked ? Constant.IconSmall : Constant.IconLarge;
+                _coreInvoker.SaveUserSettings();
+            };
+
+            checkBoxIconSize1.CheckedChanged += (s, e) =>
+            {
+                _coreInvoker.UserSettings.ReadyState.IconSize = checkBoxIconSize1.Checked ? Constant.IconSmall : Constant.IconLarge;
+                _coreInvoker.SaveUserSettings();
+            };
+
+            checkBoxIconSize2.CheckedChanged += (s, e) =>
+            {
+                _coreInvoker.UserSettings.TargetState.IconSize = checkBoxIconSize2.Checked ? Constant.IconSmall : Constant.IconLarge;
+                _coreInvoker.SaveUserSettings();
+            };
+
+            radioButtonDisable0.CheckedChanged += (s, e) =>
+            {
+                if (!radioButtonDisable0.Checked) return;
+
+                _coreInvoker.UserSettings.ResetState.TransparentMode = TransparentModeType.Disable;
+                _coreInvoker.SaveUserSettings();
+            };
+
+            radioButtonDisable1.CheckedChanged += (s, e) =>
+            {
+                if (!radioButtonDisable1.Checked) return;
+
+                _coreInvoker.UserSettings.ReadyState.TransparentMode = TransparentModeType.Disable;
+                _coreInvoker.SaveUserSettings();
+            };
+
+            radioButtonDisable2.CheckedChanged += (s, e) =>
+            {
+                if (!radioButtonDisable2.Checked) return;
+
+                _coreInvoker.UserSettings.TargetState.TransparentMode = TransparentModeType.Disable;
+                _coreInvoker.SaveUserSettings();
+            };
+
+            radioButtonTransparent0.CheckedChanged += (s, e) =>
+            {
+                if (!radioButtonTransparent0.Checked) return;
+
+                _coreInvoker.UserSettings.ResetState.TransparentMode = TransparentModeType.Transparent;
+                _coreInvoker.SaveUserSettings();
+            };
+
+            radioButtonTransparent1.CheckedChanged += (s, e) =>
+            {
+                if (!radioButtonTransparent1.Checked) return;
+
+                _coreInvoker.UserSettings.ReadyState.TransparentMode = TransparentModeType.Transparent;
+                _coreInvoker.SaveUserSettings();
+            };
+
+            radioButtonTransparent2.CheckedChanged += (s, e) =>
+            {
+                if (!radioButtonTransparent2.Checked) return;
+
+                _coreInvoker.UserSettings.TargetState.TransparentMode = TransparentModeType.Transparent;
+                _coreInvoker.SaveUserSettings();
+            };
+
+            radioButtonBlur0.CheckedChanged += (s, e) =>
+            {
+                if (!radioButtonBlur0.Checked) return;
+
+                _coreInvoker.UserSettings.ResetState.TransparentMode = TransparentModeType.Blur;
+                _coreInvoker.SaveUserSettings();
+            };
+
+            radioButtonBlur1.CheckedChanged += (s, e) =>
+            {
+                if (!radioButtonBlur1.Checked) return;
+
+                _coreInvoker.UserSettings.ReadyState.TransparentMode = TransparentModeType.Blur;
+                _coreInvoker.SaveUserSettings();
+            };
+
+            radioButtonBlur2.CheckedChanged += (s, e) =>
+            {
+                if (!radioButtonBlur2.Checked) return;
+
+                _coreInvoker.UserSettings.TargetState.TransparentMode = TransparentModeType.Blur;
+                _coreInvoker.SaveUserSettings();
+            };
+
+            #endregion
+
+            LoadSettings();
+
+            #endregion
+        }
+
+        private void LoadSettings()
+        {
+            checkBoxIsAutoHide0.Checked = _coreInvoker.UserSettings.ResetState.IsAutoHide;
+            checkBoxHideTaskbar0.Checked = _coreInvoker.UserSettings.ResetState.HideTaskbarCompletely;
+            checkBoxIconSize0.Checked = _coreInvoker.UserSettings.ResetState.IconSize == Constant.IconSmall;
+
+            switch (_coreInvoker.UserSettings.ResetState.TransparentMode)
+            {
+                case TransparentModeType.Disable:
+                    radioButtonDisable0.Checked = true;
+                    break;
+                case TransparentModeType.Transparent:
+                    radioButtonTransparent0.Checked = true;
+                    break;
+                case TransparentModeType.Blur:
+                    radioButtonBlur0.Checked = true;
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException();
+            }
+
+
+            checkBoxIsAutoHide1.Checked = _coreInvoker.UserSettings.ReadyState.IsAutoHide;
+            checkBoxHideTaskbar1.Checked = _coreInvoker.UserSettings.ReadyState.HideTaskbarCompletely;
+            checkBoxIconSize1.Checked = _coreInvoker.UserSettings.ReadyState.IconSize == Constant.IconSmall;
+
+            switch (_coreInvoker.UserSettings.ReadyState.TransparentMode)
+            {
+                case TransparentModeType.Disable:
+                    radioButtonDisable1.Checked = true;
+                    break;
+                case TransparentModeType.Transparent:
+                    radioButtonTransparent1.Checked = true;
+                    break;
+                case TransparentModeType.Blur:
+                    radioButtonBlur1.Checked = true;
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException();
+            }
+
+            checkBoxIsAutoHide2.Checked = _coreInvoker.UserSettings.TargetState.IsAutoHide;
+            checkBoxHideTaskbar2.Checked = _coreInvoker.UserSettings.TargetState.HideTaskbarCompletely;
+            checkBoxIconSize2.Checked = _coreInvoker.UserSettings.TargetState.IconSize == Constant.IconSmall;
+
+            switch (_coreInvoker.UserSettings.TargetState.TransparentMode)
+            {
+                case TransparentModeType.Disable:
+                    radioButtonDisable2.Checked = true;
+                    break;
+                case TransparentModeType.Transparent:
+                    radioButtonTransparent2.Checked = true;
+                    break;
+                case TransparentModeType.Blur:
+                    radioButtonBlur2.Checked = true;
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException();
+            }
         }
 
 
