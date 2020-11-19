bd9b8d6e3015a4e16b065114c525caed7cdf7d72
Chanple <chanple@outlook.com>
2020-11-19 16:38:32 +0800

    ~

--- SmartTaskbar.UI/Views/MainSettingForm.Designer.cs
+++ SmartTaskbar.UI/Views/MainSettingForm.Designer.cs
@@ -29,10 +29,18 @@ namespace SmartTaskbar.UI.Views
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // MainSettingForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 17F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "MainSettingForm";
+            this.Name = "MainSettingForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "SmartTaskbar";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
