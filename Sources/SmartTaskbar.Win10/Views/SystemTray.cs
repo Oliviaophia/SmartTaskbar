d770087aad3bd992d69e6dc795d9362ae51250f3
Chanple <chanple@outlook.com>
2022-04-13 21:38:53 +0800

    Update SystemTray.cs

--- Sources/SmartTaskbar.Win10/Views/SystemTray.cs
+++ Sources/SmartTaskbar.Win10/Views/SystemTray.cs
@@ -73,7 +73,7 @@ namespace SmartTaskbar
 
             _notifyIcon = new NotifyIcon(_container)
             {
-                Text = @"SmartTaskbar v1.4.2",
+                Text = @"SmartTaskbar v1.4.3",
                 Icon = Fun.IsLightTheme() ? Resources.Logo_Black : Resources.Logo_White,
                 Visible = true
             };
